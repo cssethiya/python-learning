ae87658f4b180d79847c331475f6ffa9424caffe
cssethiya <94955240+cssethiya@users.noreply.github.com>
2021-11-24 14:51:07 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,12 @@
+class teacher_info:
+    #def __init__(self,name,age,degree,position,class_assigned):
+        #self.age = age
+        #self.degree = degree
+        #self.position = position
+        #self.class_assigned = class_assigned 
+    
+    def salary(self,salary):
+        print("The salary is: ",salary)
+
+    def department(self,department):
+        print("The department is: ", department)
